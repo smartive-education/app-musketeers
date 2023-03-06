b40f425b68ea3371764d183066e9e9080009b4b6
André Abt <info@andre-abt.com>
2023-03-06 13:24:46 +0100

    feat: #67 Add timeline components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
 import Head from 'next/head';
-import { Container } from '@smartive-education/design-system-component-library-musketeers';
+import {
+  Container,
+  Avatar,
+  AvatarSize,
+  Interaction,
+  InteractionType,
+  Share,
+  Hashtag,
+} from '@smartive-education/design-system-component-library-musketeers';
 import Mumble from '../components/mumble';
 import MumbleAdd from '../components/mumble-add';
 
@@ -16,10 +24,62 @@ export default function PageHome() {
           Voluptatem qui cumque voluptatem quia tempora dolores distinctio vel repellat dicta.
         </h2>
         <div className="mb-s">
-          <MumbleAdd />
+          <MumbleAdd
+            title={'Hey, was gibt’s neues?'}
+            avatar={
+              <Avatar
+                alt="Display Name @displayName"
+                showBorder
+                size={AvatarSize.M}
+                src="https://randompicturegenerator.com/img/people-generator/gd121f56d8674f28d00ce9f1c44686e7a9bee58b8d33a3c57daaada1fa493c214290f9490833d1ff18f4ee16cd5298e1f_640.jpg"
+              />
+            }
+          />
         </div>
         <div>
-          <Mumble />
+          <Mumble
+            avatar={
+              <Avatar
+                alt="Display Name @displayName"
+                showBorder
+                size={AvatarSize.M}
+                src="https://randompicturegenerator.com/img/people-generator/gd121f56d8674f28d00ce9f1c44686e7a9bee58b8d33a3c57daaada1fa493c214290f9490833d1ff18f4ee16cd5298e1f_640.jpg"
+              />
+            }
+            displayName="Hans Muster"
+            footer={
+              <>
+                {/* <CommentInteractionComponent />
+                <LikeInteractionComponent /> */}
+                <Interaction onClick={(event) => event} type={InteractionType.DEFAULT}>
+                  <Share />
+                  Copy link
+                </Interaction>
+              </>
+            }
+            onClickTimestamp={() => {}}
+            onClickUserName={() => {}}
+            timestamp="timestamp"
+            userName="hansmuster"
+          >
+            <>
+              <div>
+                Sed at eleifend erat. Ut mattis malesuada tristique. Aliquam eget ultricies ipsum. Quisque rutrum orci non
+                risus dignissim, a semper tortor ultricies. Nam commodo lacus mi, molestie malesuada mi scelerisque eu.
+                Aenean et pellentesque lectus, in iaculis sem. In ac efficitur lectus. Quisque consectetur est libero, eu
+                sagittis magna vestibulum a. Etiam consectetur lobortis ipsum et bibendum. Maecenas id purus quam.
+              </div>
+              <div className="flex gap-xs">
+                <Hashtag label="myhashtag" onClick={(event) => event} />
+                <Hashtag label="myhashtag2" onClick={(event) => event} />
+              </div>
+              <img
+                alt=""
+                className="block rounded-default"
+                src="https://randompicturegenerator.com/img/people-generator/gd121f56d8674f28d00ce9f1c44686e7a9bee58b8d33a3c57daaada1fa493c214290f9490833d1ff18f4ee16cd5298e1f_640.jpg"
+              />
+            </>
+          </Mumble>
         </div>
       </Container>
     </>
